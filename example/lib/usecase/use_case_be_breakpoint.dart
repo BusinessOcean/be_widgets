b11a6f3e3305c9b2dd4b18939b841291aaf346cc
zendesk-souravpandit <sourav.pandit@zendesk.com>
2025-09-10 22:57:33 +0530

    feat: example code

--- example/lib/usecase/use_case_be_breakpoint.dart
+++ example/lib/usecase/use_case_be_breakpoint.dart
@@ -1,6 +1,5 @@
 import 'package:be_widgets/be_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'BeBreakpoint', path: '/', type: BeBreakpoint)
